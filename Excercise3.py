ee227593559637ab992b0513029758d176c28fd4
nagyimregergely <80952724+nagyimregergely@users.noreply.github.com>
2021-03-19 13:17:14 +0100

    Add files via upload

---
+++ Excercise3.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 11 19:08:22 2021
+
+@author: INA005
+"""
+import matplotlib.pyplot as plt
+import csv
+
+
+statData = "C:\\Users\\ina005\OneDrive - Winterthur Gas & Diesel Ltd\\Desktop\\2021\\Imre\\Python\\Ex4.csv"
+
+with open(statData,"r") as rawdata:
+    content = rawdata.read()
+    # print(content)
+    # print(type(content))
+    # header = content[0:25]
+    # print(header)
+    # content = csv.reader(content, delimiter=',')
+
+
+
+
+
+    
